4cf1ad3c88773f9eaf0369727e74b0b31f1f1358
Abiud Orina <abiudrn@gmail.com>
2023-10-13 19:49:14 +0300

    feat: add styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,21 @@
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
 import Link from 'next/link';
-import Image from 'next/image';
-import Script from 'next/script';
 import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
     <Layout>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+      <div className={styles.container}>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <main>
+          <h2 className={styles.title}>
+            First Post <Link href="/">Back to home</Link>
+          </h2>
+        </main>
+      </div>
     </Layout>
   );
 }
